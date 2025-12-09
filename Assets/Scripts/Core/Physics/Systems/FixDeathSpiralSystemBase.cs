bcee20557feec3667a409abe2ba56a1d68dba56d
Tudor Gheorghe <mr.tudor.gheorghe@gmail.com>
2025-12-09 23:48:50 +0000

    [PROTOTYPE] Set fixed rate sampler to 120f.

--- Assets/Scripts/Core/Physics/Systems/FixDeathSpiralSystemBase.cs
+++ Assets/Scripts/Core/Physics/Systems/FixDeathSpiralSystemBase.cs
@@ -1,13 +1,15 @@
+using PotionCraft.Core.Physics.Groups;
 using Unity.Burst;
 using Unity.Entities;
 using static Unity.Entities.RateUtils;
 
+[UpdateInGroup(typeof(CustomPhysicsInitializationGroup), OrderFirst = true)]
 partial class FixDeathSpiralSystemBase : SystemBase
 {
     protected override void OnCreate()
     {
         var fixedStepGroup = World.GetExistingSystemManaged<FixedStepSimulationSystemGroup>();
-        fixedStepGroup.RateManager = new FixedRateSimpleManager(1f / 60f);
+        fixedStepGroup.RateManager = new FixedRateSimpleManager(1f / 120f);
     }
 
     protected override void OnUpdate()
