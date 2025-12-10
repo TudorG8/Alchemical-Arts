0cfa8b661a47f9a2a593357264224266b89e122e
Tudor Gheorghe <mr.tudor.gheorghe@gmail.com>
2025-12-10 17:55:32 +0000

    [PROTOTYPE] FixDeathSpirral now deactivates the normal physics loop.

--- Assets/Scripts/Core/Physics/Systems/FixDeathSpiralSystemBase.cs
+++ Assets/Scripts/Core/Physics/Systems/FixDeathSpiralSystemBase.cs
@@ -1,19 +1,24 @@
 using PotionCraft.Core.Physics.Groups;
-using Unity.Burst;
 using Unity.Entities;
+using Unity.Physics.Systems;
 using static Unity.Entities.RateUtils;
 
 [UpdateInGroup(typeof(CustomPhysicsInitializationGroup), OrderFirst = true)]
 partial class FixDeathSpiralSystemBase : SystemBase
 {
-    protected override void OnCreate()
-    {
-        var fixedStepGroup = World.GetExistingSystemManaged<FixedStepSimulationSystemGroup>();
-        fixedStepGroup.RateManager = new FixedRateSimpleManager(1f / 120f);
-    }
+	protected override void OnCreate()
+	{
+		var fixedStepGroup = World.GetExistingSystemManaged<FixedStepSimulationSystemGroup>();
+		fixedStepGroup.RateManager = new FixedRateSimpleManager(1f / 120f);
 
-    protected override void OnUpdate()
-    {
-        Enabled = false;
-    }
+		
+	}
+
+	protected override void OnUpdate()
+	{
+		var group = World.GetExistingSystemManaged<PhysicsSystemGroup>();
+		group.Enabled = false;
+
+		Enabled = false;
+	}
 }
