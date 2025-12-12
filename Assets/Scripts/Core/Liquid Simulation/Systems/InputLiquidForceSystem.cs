f83a85accdfb78f08707d6aa7f3e1e94cf3be0b8
Tudor Gheorghe <mr.tudor.gheorghe@gmail.com>
2025-12-12 21:21:24 +0000

    [PROTOTYPE] Added a system to use user input to pickup liquid.

---
+++ Assets/Scripts/Core/Liquid Simulation/Systems/InputLiquidForceSystem.cs
@@ -0,0 +1,100 @@
+using PotionCraft.Gameplay.Authoring;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[UpdateInGroup(typeof(LiquidPhysicsGroup))]
+[UpdateAfter(typeof(PopulateLiquidPositionsSystem))]
+partial struct InputLiquidForceSystem : ISystem
+{
+	private SystemHandle populateLiquidPositionsSystemHandle;
+
+	private float interactionStrength;
+
+	private float interactionRadius;
+
+
+	[BurstCompile]
+	public void OnCreate(ref SystemState state)
+	{
+		interactionRadius = 3f;
+		interactionStrength = 200f;
+		populateLiquidPositionsSystemHandle = state.WorldUnmanaged.GetExistingUnmanagedSystem<PopulateLiquidPositionsSystem>();
+		state.RequireForUpdate<InputDataConfig>();
+	}
+
+	[BurstCompile]
+	public void OnUpdate(ref SystemState state)
+	{
+		ref var populateLiquidPositionsSystem = ref state.WorldUnmanaged.GetUnsafeSystemRef<PopulateLiquidPositionsSystem>(populateLiquidPositionsSystemHandle);
+		var inputData = SystemAPI.GetSingleton<InputDataConfig>();
+		
+		var isPullInteraction = inputData.primaryPressed;
+		var isPushInteraction = inputData.secondaryPressed;
+		var currInteractStrength = 0f;
+		if (isPushInteraction || isPullInteraction)
+		{
+			currInteractStrength = isPushInteraction ? -interactionStrength : interactionStrength;
+		}
+
+		var applyGravityJob = new ApplyUserInputJob
+		{
+			velocities = populateLiquidPositionsSystem.velocityBuffer,
+			positions = populateLiquidPositionsSystem.positionBuffer,
+			input = inputData.worldPosition,
+			radius = interactionRadius,
+			strength = currInteractStrength,
+			deltaTime = SystemAPI.Time.DeltaTime,
+		};
+		var applyHandle = applyGravityJob.ScheduleParallel(state.Dependency);
+		applyHandle.Complete();
+	}
+
+	[BurstCompile]
+	public void OnDestroy(ref SystemState state)
+	{
+		
+	}
+
+	[BurstCompile]
+	[WithAll(typeof(_LiquidTag))]
+	public partial struct ApplyUserInputJob : IJobEntity
+	{
+		public NativeArray<float2> velocities;
+
+		[ReadOnly]
+		public NativeArray<float2> positions;
+
+		[ReadOnly]
+		public float2 input;
+
+		[ReadOnly]
+		public float radius;
+		
+		[ReadOnly]
+		public float strength;
+
+		[ReadOnly]
+		public float deltaTime;
+
+
+		void Execute(
+			[EntityIndexInQuery] int index,
+			in _LiquidTag _)
+		{
+			var interactionForce = float2.zero;
+			var offset = input - positions[index];
+			var sqrDst = math.dot(offset, offset);
+			if (sqrDst < radius * radius)
+			{
+				var dst = math.sqrt(sqrDst);
+				var dir = dst <= float.Epsilon ? float2.zero : offset / dst;
+				var centreT = 1 - dst/radius;
+				interactionForce += (dir * strength - velocities[index]) * centreT;
+			}
+
+			velocities[index] += interactionForce * deltaTime;
+		}
+	}
+}
